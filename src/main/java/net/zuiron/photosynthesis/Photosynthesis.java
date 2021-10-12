560d9b74767094768c208eba1e7e65404f29780b
Zuiron <martinfloden90@gmail.com>
2021-10-12 21:49:17 +0200

    .

--- src/main/java/net/zuiron/photosynthesis/Photosynthesis.java
+++ src/main/java/net/zuiron/photosynthesis/Photosynthesis.java
@@ -1,14 +1,14 @@
-package net.fabricmc.example;
+package net.zuiron.photosynthesis;
 
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class ExampleMod implements ModInitializer {
+public class Photosynthesis implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("modid");
+	public static final Logger LOGGER = LogManager.getLogger("photosynthesis");
 
 	@Override
 	public void onInitialize() {
--- src/main/java/net/zuiron/photosynthesis/mixin/PhotosynthesisMixin.java
+++ src/main/java/net/zuiron/photosynthesis/mixin/PhotosynthesisMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package net.zuiron.photosynthesis.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import net.zuiron.photosynthesis.Photosynthesis;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class PhotosynthesisMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		Photosynthesis.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
