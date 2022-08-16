11ed71de74949154ea7cb28f2f2389fc8aaf6ded
Zuiron <martinfloden90@gmail.com>
2022-08-16 03:33:05 +0200

    fixerinos!

--- src/main/java/net/zuiron/photosynthesis/block/CustomCropBlock.java
+++ src/main/java/net/zuiron/photosynthesis/block/CustomCropBlock.java
@@ -27,7 +27,53 @@ public class CustomCropBlock extends CropBlock {
     @Override
     protected ItemConvertible getSeedsItem() {
 
-        if(seed == "turnip_crop") { return ModItems.TURNIP; }
+        if(seed == "tomato_crop") { return ModItems.TOMATO; }
+        else if(seed == "basil_crop") { return ModItems.BASIL; }
+        else if(seed == "oregano_crop") { return ModItems.OREGANO; }
+        else if(seed == "strawberry_crop") { return ModItems.STRAWBERRY; }
+        else if(seed == "oat_crop") { return ModItems.OAT; }
+        else if(seed == "jalapeno_crop") { return ModItems.JALAPENO; }
+        else if(seed == "chili_crop") { return ModItems.CHILI; }
+        else if(seed == "cucumber_crop") { return ModItems.CUCUMBER; }
+        else if(seed == "onion_crop") { return ModItems.ONION; }
+        else if(seed == "leek_crop") { return ModItems.LEEK; }
+        else if(seed == "celery_crop") { return ModItems.CELERY; }
+        else if(seed == "peas_crop") { return ModItems.PEAS; }
+        else if(seed == "sweet_potato_crop") { return ModItems.SWEET_POTATO; }
+        else if(seed == "asparagus_crop") { return ModItems.ASPARAGUS; }
+        else if(seed == "scallion_crop") { return ModItems.SCALLION; }
+        else if(seed == "garlic_crop") { return ModItems.GARLIC; }
+        else if(seed == "chive_crop") { return ModItems.CHIVE; }
+        else if(seed == "broccoli_crop") { return ModItems.BROCCOLI; }
+        else if(seed == "cauliflower_crop") { return ModItems.CAULIFLOWER; }
+        else if(seed == "corn_crop") { return ModItems.CORN; }
+        else if(seed == "black_pepper_crop") { return ModItems.BLACK_PEPPER; }
+        else if(seed == "cabbage_crop") { return ModItems.CABBAGE; }
+        else if(seed == "bellpepper_crop") { return ModItems.RED_BELLPEPPER; }
+        else if(seed == "turnip_crop") { return ModItems.TURNIP; }
+        else if(seed == "rutabaga_crop") { return ModItems.RUTABAGA; }
+        else if(seed == "canola_crop") { return ModItems.CANOLA; }
+        else if(seed == "barley_crop") { return ModItems.BARLEY; }
+        else if(seed == "cotton_crop") { return ModItems.COTTON; }
+        else if(seed == "sugarbeet_crop") { return ModItems.SUGARBEET; }
+        else if(seed == "rice_crop") { return ModItems.RICE; }
+        else if(seed == "sugarcane_crop") { return Items.SUGAR_CANE; }
+        else if(seed == "soybean_crop") { return ModItems.SOYBEAN; }
+        else if(seed == "spinach_crop") { return ModItems.SPINACH; }
+        else if(seed == "arrowroot_crop") { return ModItems.ARROWROOT; }
+        else if(seed == "artichoke_crop") { return ModItems.ARTICHOKE; }
+        else if(seed == "brussels_sprouts_crop") { return ModItems.BRUSSELS_SPROUTS; }
+        else if(seed == "cassava_crop") { return ModItems.CASSAVA; }
+        else if(seed == "eggplant_crop") { return ModItems.EGGPLANT; }
+        else if(seed == "sunflower_crop") { return ModItems.SUNFLOWER; }
+        else if(seed == "jicama_crop") { return ModItems.JICAMA; }
+        else if(seed == "kale_crop") { return ModItems.KALE; }
+        else if(seed == "kohlrabi_crop") { return ModItems.KOHLRABI; }
+        else if(seed == "lettuce_crop") { return ModItems.LETTUCE; }
+        else if(seed == "okra_crop") { return ModItems.OKRA; }
+        else if(seed == "parsnip_crop") { return ModItems.PARSNIP; }
+        else if(seed == "radish_crop") { return ModItems.RADISH; }
+        else if(seed == "rhubarb_crop") { return ModItems.RHUBARB; }
 
         else {
             return Items.AIR;
--- src/main/java/net/zuiron/photosynthesis/block/ModBlocks.java
+++ src/main/java/net/zuiron/photosynthesis/block/ModBlocks.java
@@ -20,53 +20,53 @@ public class ModBlocks {
 
 
     // CROP BLOCKS -----------------------------------------------------------------------------------------------------
-    public static final CropBlock TOMATO_CROP = registerCropBlockSimple("tomato_crop", ModItems.TOMATO_SEEDS);
-    public static final CropBlock BASIL_CROP = registerCropBlockSimple("basil_crop", ModItems.BASIL_SEEDS);
-    public static final CropBlock OREGANO_CROP = registerCropBlockSimple("oregano_crop", ModItems.OREGANO_SEEDS);
-    public static final CropBlock STRAWBERRY_CROP = registerCropBlockSimple("strawberry_crop", ModItems.STRAWBERRY_SEEDS);
-    public static final CropBlock OAT_CROP = registerCropBlockSimple("oat_crop", ModItems.OAT_SEEDS);
-    public static final CropBlock JALAPENO_CROP = registerCropBlockSimple("jalapeno_crop", ModItems.JALAPENO_SEEDS);
-    public static final CropBlock CHILI_CROP = registerCropBlockSimple("chili_crop", ModItems.CHILI_SEEDS);
-    public static final CropBlock CUCUMBER_CROP = registerCropBlockSimple("cucumber_crop", ModItems.CUCUMBER_SEEDS);
-    public static final CropBlock ONION_CROP = registerCropBlockSimple("onion_crop", ModItems.ONION_SEEDS);
-    public static final CropBlock LEEK_CROP = registerCropBlockSimple("leek_crop", ModItems.LEEK_SEEDS);
-    public static final CropBlock CELERY_CROP = registerCropBlockSimple("celery_crop", ModItems.CELERY_SEEDS);
-    public static final CropBlock PEAS_CROP = registerCropBlockSimple("peas_crop", ModItems.PEAS_SEEDS);
-    public static final CropBlock SWEET_POTATO_CROP = registerCropBlockSimple("sweet_potato_crop", ModItems.SWEET_POTATO_SEEDS);
-    public static final CropBlock ASPARAGUS_CROP = registerCropBlockSimple("asparagus_crop", ModItems.ASPARAGUS_SEEDS);
-    public static final CropBlock SCALLION_CROP = registerCropBlockSimple("scallion_crop", ModItems.SCALLION_SEEDS);
-    public static final CropBlock GARLIC_CROP = registerCropBlockSimple("garlic_crop", ModItems.GARLIC_SEEDS);
-    public static final CropBlock CHIVE_CROP = registerCropBlockSimple("chive_crop", ModItems.CHIVE_SEEDS);
-    public static final CropBlock BROCCOLI_CROP = registerCropBlockSimple("broccoli_crop", ModItems.BROCCOLI_SEEDS);
-    public static final CropBlock CAULIFLOWER_CROP = registerCropBlockSimple("cauliflower_crop", ModItems.CAULIFLOWER_SEEDS);
-    public static final CropBlock CORN_CROP = registerCropBlockSimple("corn_crop", ModItems.CORN_SEEDS);
-    public static final CropBlock BLACK_PEPPER_CROP = registerCropBlockSimple("black_pepper_crop", ModItems.BLACK_PEPPER_SEEDS);
-    public static final CropBlock CABBAGE_CROP = registerCropBlockSimple("cabbage_crop", ModItems.CABBAGE_SEEDS);
-    public static final CropBlock BELLPEPPER_CROP = registerCropBlockSimple("bellpepper_crop", ModItems.BELLPEPPER_SEEDS);
-    public static final CropBlock TURNIP_CROP = registerCropBlockSimple("turnip_crop", ModItems.TURNIP_SEEDS);
-    public static final CropBlock RUTABAGA_CROP = registerCropBlockSimple("rutabaga_crop", ModItems.RUTABAGA_SEEDS);
-    public static final CropBlock CANOLA_CROP = registerCropBlockSimple("canola_crop", ModItems.CANOLA_SEEDS);
-    public static final CropBlock BARLEY_CROP = registerCropBlockSimple("barley_crop", ModItems.BARLEY_SEEDS);
-    public static final CropBlock COTTON_CROP = registerCropBlockSimple("cotton_crop", ModItems.COTTON_SEEDS);
-    public static final CropBlock SUGARBEET_CROP = registerCropBlockSimple("sugarbeet_crop", ModItems.SUGARBEET_SEEDS);
-    public static final CropBlock RICE_CROP = registerCropBlockSimple("rice_crop", ModItems.RICE_SEEDS);
-    public static final CropBlock SUGARCANE_CROP = registerCropBlockSimple("sugarcane_crop", ModItems.SUGARCANE_SEEDS);
-    public static final CropBlock SOYBEAN_CROP = registerCropBlockSimple("soybean_crop", ModItems.SOYBEAN_SEEDS);
-    public static final CropBlock SPINACH_CROP = registerCropBlockSimple("spinach_crop", ModItems.SPINACH_SEEDS);
-    public static final CropBlock ARROWROOT_CROP = registerCropBlockSimple("arrowroot_crop", ModItems.ARROWROOT_SEEDS);
-    public static final CropBlock ARTICHOKE_CROP = registerCropBlockSimple("artichoke_crop", ModItems.ARTICHOKE_SEEDS);
-    public static final CropBlock BRUSSELS_SPROUTS_CROP = registerCropBlockSimple("brussels_sprouts_crop", ModItems.BRUSSELS_SPROUTS_SEEDS);
-    public static final CropBlock CASSAVA_CROP = registerCropBlockSimple("cassava_crop", ModItems.CASSAVA_SEEDS);
-    public static final CropBlock EGGPLANT_CROP = registerCropBlockSimple("eggplant_crop", ModItems.EGGPLANT_SEEDS);
-    public static final CropBlock SUNFLOWER_CROP = registerCropBlockSimple("sunflower_crop", ModItems.SUNFLOWER_SEEDS);
-    public static final CropBlock JICAMA_CROP = registerCropBlockSimple("jicama_crop", ModItems.JICAMA_SEEDS);
-    public static final CropBlock KALE_CROP = registerCropBlockSimple("kale_crop", ModItems.KALE_SEEDS);
-    public static final CropBlock KOHLRABI_CROP = registerCropBlockSimple("kohlrabi_crop", ModItems.KOHLRABI_SEEDS);
-    public static final CropBlock LETTUCE_CROP = registerCropBlockSimple("lettuce_crop", ModItems.LETTUCE_SEEDS);
-    public static final CropBlock OKRA_CROP = registerCropBlockSimple("okra_crop", ModItems.OKRA_SEEDS);
-    public static final CropBlock PARSNIP_CROP = registerCropBlockSimple("parsnip_crop", ModItems.PARSNIP_SEEDS);
-    public static final CropBlock RADISH_CROP = registerCropBlockSimple("radish_crop", ModItems.RADISH_SEEDS);
-    public static final CropBlock RHUBARB_CROP = registerCropBlockSimple("rhubarb_crop", ModItems.RHUBARB_SEEDS);
+    public static final CropBlock TOMATO_CROP = registerCropBlockSimple("tomato_crop");
+    public static final CropBlock BASIL_CROP = registerCropBlockSimple("basil_crop");
+    public static final CropBlock OREGANO_CROP = registerCropBlockSimple("oregano_crop");
+    public static final CropBlock STRAWBERRY_CROP = registerCropBlockSimple("strawberry_crop");
+    public static final CropBlock OAT_CROP = registerCropBlockSimple("oat_crop");
+    public static final CropBlock JALAPENO_CROP = registerCropBlockSimple("jalapeno_crop");
+    public static final CropBlock CHILI_CROP = registerCropBlockSimple("chili_crop");
+    public static final CropBlock CUCUMBER_CROP = registerCropBlockSimple("cucumber_crop");
+    public static final CropBlock ONION_CROP = registerCropBlockSimple("onion_crop");
+    public static final CropBlock LEEK_CROP = registerCropBlockSimple("leek_crop");
+    public static final CropBlock CELERY_CROP = registerCropBlockSimple("celery_crop");
+    public static final CropBlock PEAS_CROP = registerCropBlockSimple("peas_crop");
+    public static final CropBlock SWEET_POTATO_CROP = registerCropBlockSimple("sweet_potato_crop");
+    public static final CropBlock ASPARAGUS_CROP = registerCropBlockSimple("asparagus_crop");
+    public static final CropBlock SCALLION_CROP = registerCropBlockSimple("scallion_crop");
+    public static final CropBlock GARLIC_CROP = registerCropBlockSimple("garlic_crop");
+    public static final CropBlock CHIVE_CROP = registerCropBlockSimple("chive_crop");
+    public static final CropBlock BROCCOLI_CROP = registerCropBlockSimple("broccoli_crop");
+    public static final CropBlock CAULIFLOWER_CROP = registerCropBlockSimple("cauliflower_crop");
+    public static final CropBlock CORN_CROP = registerCropBlockSimple("corn_crop");
+    public static final CropBlock BLACK_PEPPER_CROP = registerCropBlockSimple("black_pepper_crop");
+    public static final CropBlock CABBAGE_CROP = registerCropBlockSimple("cabbage_crop");
+    public static final CropBlock BELLPEPPER_CROP = registerCropBlockSimple("bellpepper_crop");
+    public static final CropBlock TURNIP_CROP = registerCropBlockSimple("turnip_crop");
+    public static final CropBlock RUTABAGA_CROP = registerCropBlockSimple("rutabaga_crop");
+    public static final CropBlock CANOLA_CROP = registerCropBlockSimple("canola_crop");
+    public static final CropBlock BARLEY_CROP = registerCropBlockSimple("barley_crop");
+    public static final CropBlock COTTON_CROP = registerCropBlockSimple("cotton_crop");
+    public static final CropBlock SUGARBEET_CROP = registerCropBlockSimple("sugarbeet_crop");
+    public static final CropBlock RICE_CROP = registerCropBlockSimple("rice_crop");
+    public static final CropBlock SUGARCANE_CROP = registerCropBlockSimple("sugarcane_crop");
+    public static final CropBlock SOYBEAN_CROP = registerCropBlockSimple("soybean_crop");
+    public static final CropBlock SPINACH_CROP = registerCropBlockSimple("spinach_crop");
+    public static final CropBlock ARROWROOT_CROP = registerCropBlockSimple("arrowroot_crop");
+    public static final CropBlock ARTICHOKE_CROP = registerCropBlockSimple("artichoke_crop");
+    public static final CropBlock BRUSSELS_SPROUTS_CROP = registerCropBlockSimple("brussels_sprouts_crop");
+    public static final CropBlock CASSAVA_CROP = registerCropBlockSimple("cassava_crop");
+    public static final CropBlock EGGPLANT_CROP = registerCropBlockSimple("eggplant_crop");
+    public static final CropBlock SUNFLOWER_CROP = registerCropBlockSimple("sunflower_crop");
+    public static final CropBlock JICAMA_CROP = registerCropBlockSimple("jicama_crop");
+    public static final CropBlock KALE_CROP = registerCropBlockSimple("kale_crop");
+    public static final CropBlock KOHLRABI_CROP = registerCropBlockSimple("kohlrabi_crop");
+    public static final CropBlock LETTUCE_CROP = registerCropBlockSimple("lettuce_crop");
+    public static final CropBlock OKRA_CROP = registerCropBlockSimple("okra_crop");
+    public static final CropBlock PARSNIP_CROP = registerCropBlockSimple("parsnip_crop");
+    public static final CropBlock RADISH_CROP = registerCropBlockSimple("radish_crop");
+    public static final CropBlock RHUBARB_CROP = registerCropBlockSimple("rhubarb_crop");
 
     /*public static final CropBlock RHUBARB_CROP = registerCropBlock("rhubarb_crop",
             new CustomCropBlock(AbstractBlock.Settings.of(Material.PLANT)
@@ -116,7 +116,7 @@ public class ModBlocks {
         return Registry.register(Registry.BLOCK, new Identifier(Photosynthesis.MOD_ID, name), block);
     }
 
-    private static CropBlock registerCropBlockSimple(String name, Item item){
+    private static CropBlock registerCropBlockSimple(String name){
         return Registry.register(Registry.BLOCK, new Identifier(Photosynthesis.MOD_ID, name),
                 new CustomCropBlock(AbstractBlock.Settings.of(Material.PLANT)
                 .nonOpaque().noCollision().ticksRandomly().breakInstantly().sounds(BlockSoundGroup.CROP),
