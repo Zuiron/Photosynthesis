adff0b50860d094141266a3e25a72db5118cef53
Zuiron <martinfloden90@gmail.com>
2023-02-08 16:13:45 +0100

    cinnamon tree smaller

--- src/main/java/net/zuiron/photosynthesis/world/feature/ModConfiguredFeatures.java
+++ src/main/java/net/zuiron/photosynthesis/world/feature/ModConfiguredFeatures.java
@@ -68,9 +68,9 @@ public class ModConfiguredFeatures {
     public static final RegistryEntry<ConfiguredFeature<TreeFeatureConfig, ?>> CINNAMON_TREE =
             ConfiguredFeatures.register("cinnamon_tree", Feature.TREE, new TreeFeatureConfig.Builder(
                     BlockStateProvider.of(ModBlocks.CINNAMON_LOG),
-                    new StraightTrunkPlacer(5, 6, 3),
+                    new StraightTrunkPlacer(3, 4, 2),
                     BlockStateProvider.of(ModBlocks.CINNAMON_LEAVES),
-                    new BlobFoliagePlacer(ConstantIntProvider.create(2), ConstantIntProvider.create(0), 4),
+                    new BlobFoliagePlacer(ConstantIntProvider.create(2), ConstantIntProvider.create(0), 3),
                     new TwoLayersFeatureSize(1, 0, 2)).build());
 
     public static final RegistryEntry<PlacedFeature> CINNAMON_CHECKED = PlacedFeatures.register("cinnamon_checked",
