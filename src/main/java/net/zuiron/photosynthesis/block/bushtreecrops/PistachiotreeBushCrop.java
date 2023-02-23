54928b0e5ba684841b09854dd1dccfbda3c89744
Zuiron <martinfloden90@gmail.com>
2023-02-23 23:08:38 +0100

    fruit tree fruit growthrate halved, and age to 0 instead of 1 if bonemealed.

--- src/main/java/net/zuiron/photosynthesis/block/bushtreecrops/PistachiotreeBushCrop.java
+++ src/main/java/net/zuiron/photosynthesis/block/bushtreecrops/PistachiotreeBushCrop.java
@@ -9,15 +9,18 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
 import net.zuiron.photosynthesis.block.ModBlocks;
 import net.zuiron.photosynthesis.item.ModItems;
 
@@ -25,6 +28,16 @@ public class PistachiotreeBushCrop extends SweetBerryBushBlock {
 
     public PistachiotreeBushCrop(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        int i = (Integer)state.get(AGE); //bound default 5, now 10 for slower apple growth.
+        if (i < 3 && random.nextInt(10) == 0 && world.getBaseLightLevel(pos.up(), 0) >= 9) {
+            BlockState blockState = (BlockState)state.with(AGE, i + 1);
+            world.setBlockState(pos, blockState, 2);
+            world.emitGameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Emitter.of(blockState));
+        }
     }
 
     @Override
@@ -63,7 +76,7 @@ public class PistachiotreeBushCrop extends SweetBerryBushBlock {
             int j = 1 + world.random.nextInt(2);
             dropStack(world, pos, new ItemStack(ModItems.PISTACHIO, j + (bl ? 1 : 0)));
             world.playSound((PlayerEntity)null, pos, SoundEvents.BLOCK_SWEET_BERRY_BUSH_PICK_BERRIES, SoundCategory.BLOCKS, 1.0F, 0.8F + world.random.nextFloat() * 0.4F);
-            world.setBlockState(pos, (BlockState)state.with(AGE, 1), Block.NOTIFY_LISTENERS);
+            world.setBlockState(pos, (BlockState)state.with(AGE, 0), Block.NOTIFY_LISTENERS);
             return ActionResult.SUCCESS;
         } else {
             return super.onUse(state, world, pos, player, hand, hit);
