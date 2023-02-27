e51a35136f1c798c817e8126222762a76d513074
Zuiron <martinfloden90@gmail.com>
2023-02-27 18:09:49 +0100

    cookingpot rei WORKING

--- src/main/java/net/zuiron/photosynthesis/integration/rei/cookingpot/CookingPotRecipeCategory.java
+++ src/main/java/net/zuiron/photosynthesis/integration/rei/cookingpot/CookingPotRecipeCategory.java
@@ -7,29 +7,21 @@ import me.shedaniel.rei.api.client.gui.widgets.*;
 import me.shedaniel.rei.api.client.registry.display.DisplayCategory;
 import me.shedaniel.rei.api.common.category.CategoryIdentifier;
 import me.shedaniel.rei.api.common.entry.EntryIngredient;
-import me.shedaniel.rei.api.common.entry.EntryStack;
-import me.shedaniel.rei.api.common.util.EntryIngredients;
 import me.shedaniel.rei.api.common.util.EntryStacks;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.recipe.Ingredient;
-import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableTextContent;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.collection.DefaultedList;
 import net.zuiron.photosynthesis.Photosynthesis;
 import net.zuiron.photosynthesis.block.ModBlocks;
 import net.zuiron.photosynthesis.integration.rei.PhotosynthesisREI;
-import net.zuiron.photosynthesis.integration.rei.skillet.SkilletRecipeDisplay;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Environment(EnvType.CLIENT)
 public class CookingPotRecipeCategory implements DisplayCategory<CookingPotRecipeDisplay> {
@@ -64,35 +56,21 @@ public class CookingPotRecipeCategory implements DisplayCategory<CookingPotRecip
         List<EntryIngredient> ingredientEntries = display.getIngredientEntries();
         DefaultedList counts = display.getCounts();
 
+        //INPUTS
         if (ingredientEntries != null) {
             int[] posX = {96, 5, 23, 41, 5, 23, 41};
             int[] posY = {15, 24, 24, 24, 42, 42, 42};
 
             for (int i = 0; i < ingredientEntries.size(); i++) {
-
-                /*widgets.add(Widgets.createSlot(new Point(bgBounds.x + posX[i], bgBounds.y + posY[i]))
-                        .entries(ingredientEntries.get(i)).markInput().disableBackground());*/
-
-                /*widgets.add(Widgets.createSlot(new Point(bgBounds.x + posX[i], bgBounds.y + posY[i]))
-                        .entry(ingredientEntries.get(i).get(0)));*/
-                Slot slot = Widgets.createSlot(new Point(bgBounds.x + posX[i], bgBounds.y + posY[i]))
-                        .entry(ingredientEntries.get(i).get(0));
-                slot
-                        .disableBackground()
-                        .markInput();
-                widgets.add(slot);
-
-
-                //COUNT!!!!!!
-                Label countLabel = Widgets.createLabel(new Point(slot.getBounds().x + 16, slot.getBounds().y + 8),
-                        Text.literal(""+counts.get(i)));
-                countLabel
-                        .color(Formatting.WHITE.getColorValue())
-                        .rightAligned();
-                widgets.add(countLabel);
-
-                //slot.setFocused(countLabel);
-                countLabel.setFocused(slot);
+                if(i == 0) {
+                    //TAG ITEM
+                    widgets.add(Widgets.createSlot(new Point(bgBounds.x + posX[i], bgBounds.y + posY[i]))
+                        .entries(ingredientEntries.get(i)));
+                } else {
+                    //INPUT ITEMS
+                    Item item = ((ItemStack) ingredientEntries.get(i).get(0).getValue()).getItem();
+                    widgets.add(Widgets.createSlot(new Point(bgBounds.x + posX[i], bgBounds.y + posY[i])).markInput().entries(List.of(EntryStacks.of(new ItemStack(item, (Integer) counts.get(i))))));
+                }
             }
         }
 
