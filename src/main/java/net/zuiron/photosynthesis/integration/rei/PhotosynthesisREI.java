a1bd38cc81b64a4caa3828484222b3ecaea6ddb7
Zuiron <martinfloden90@gmail.com>
2023-03-11 19:42:58 +0100

    keg REI integration

--- src/main/java/net/zuiron/photosynthesis/integration/rei/PhotosynthesisREI.java
+++ src/main/java/net/zuiron/photosynthesis/integration/rei/PhotosynthesisREI.java
@@ -1,30 +1,46 @@
 package net.zuiron.photosynthesis.integration.rei;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import me.shedaniel.math.Point;
 import me.shedaniel.math.Rectangle;
+import me.shedaniel.rei.api.client.entry.renderer.EntryRenderer;
+import me.shedaniel.rei.api.client.gui.widgets.Tooltip;
+import me.shedaniel.rei.api.client.gui.widgets.TooltipContext;
+import me.shedaniel.rei.api.client.gui.widgets.Widget;
+import me.shedaniel.rei.api.client.gui.widgets.Widgets;
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
 import me.shedaniel.rei.api.client.registry.screen.ScreenRegistry;
+import me.shedaniel.rei.api.client.util.ClientEntryStacks;
 import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.entry.EntryStack;
 import me.shedaniel.rei.api.common.util.EntryStacks;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
+import net.minecraft.client.util.math.MatrixStack;
 import net.zuiron.photosynthesis.Photosynthesis;
 import net.zuiron.photosynthesis.block.ModBlocks;
 import net.zuiron.photosynthesis.integration.rei.cookingpot.CookingPotRecipeCategory;
 import net.zuiron.photosynthesis.integration.rei.cookingpot.CookingPotRecipeDisplay;
 import net.zuiron.photosynthesis.integration.rei.cutting_board.CuttingBoardRecipeCategory;
 import net.zuiron.photosynthesis.integration.rei.cutting_board.CuttingBoardRecipeDisplay;
+import net.zuiron.photosynthesis.integration.rei.keg.KegRecipeCategory;
+import net.zuiron.photosynthesis.integration.rei.keg.KegRecipeDisplay;
 import net.zuiron.photosynthesis.integration.rei.skillet.SkilletRecipeCategory;
 import net.zuiron.photosynthesis.integration.rei.skillet.SkilletRecipeDisplay;
 import net.zuiron.photosynthesis.recipe.CookingPotRecipe;
 import net.zuiron.photosynthesis.recipe.CuttingBoardRecipe;
+import net.zuiron.photosynthesis.recipe.KegRecipe;
 import net.zuiron.photosynthesis.recipe.SkilletRecipe;
 import net.zuiron.photosynthesis.screen.CookingPotScreen;
 import net.zuiron.photosynthesis.screen.CuttingBoardScreen;
+import net.zuiron.photosynthesis.screen.KegScreen;
 import net.zuiron.photosynthesis.screen.SkilletScreen;
+import net.zuiron.photosynthesis.screen.renderer.FluidStackRenderer;
+import net.zuiron.photosynthesis.util.FluidStack;
+import org.jetbrains.annotations.Nullable;
 
 @Environment(EnvType.CLIENT)
 public class PhotosynthesisREI implements REIClientPlugin {
@@ -32,6 +48,7 @@ public class PhotosynthesisREI implements REIClientPlugin {
     public static final CategoryIdentifier<SkilletRecipeDisplay> SKILLET = CategoryIdentifier.of(Photosynthesis.MOD_ID, "skillet");
     public static final CategoryIdentifier<CuttingBoardRecipeDisplay> CUTTINGBOARD = CategoryIdentifier.of(Photosynthesis.MOD_ID, "cuttingboard");
     public static final CategoryIdentifier<CookingPotRecipeDisplay> COOKINGPOT = CategoryIdentifier.of(Photosynthesis.MOD_ID, "cookingpot");
+    public static final CategoryIdentifier<KegRecipeDisplay> KEG = CategoryIdentifier.of(Photosynthesis.MOD_ID, "keg");
 
     public static Rectangle centeredIntoRecipeBase(Point origin, int width, int height) {
         return centeredInto(new Rectangle(origin.x, origin.y, 150, 66), width, height);
@@ -54,6 +71,10 @@ public class PhotosynthesisREI implements REIClientPlugin {
         registry.add(
                 new CookingPotRecipeCategory());
         registry.addWorkstations(COOKINGPOT, EntryStacks.of(ModBlocks.COOKINGPOT));
+
+        registry.add(
+                new KegRecipeCategory());
+        registry.addWorkstations(KEG, EntryStacks.of(ModBlocks.KEG));
     }
 
     @Override
@@ -63,6 +84,8 @@ public class PhotosynthesisREI implements REIClientPlugin {
         registry.registerRecipeFiller(CuttingBoardRecipe.class, CuttingBoardRecipe.Type.INSTANCE, CuttingBoardRecipeDisplay::new);
 
         registry.registerRecipeFiller(CookingPotRecipe.class, CookingPotRecipe.Type.INSTANCE, CookingPotRecipeDisplay::new);
+
+        registry.registerRecipeFiller(KegRecipe.class, KegRecipe.Type.INSTANCE, KegRecipeDisplay::new);
     }
 
     @Override
@@ -72,5 +95,7 @@ public class PhotosynthesisREI implements REIClientPlugin {
         registry.registerContainerClickArea(new Rectangle(99, 25, 24, 17), CuttingBoardScreen.class, CUTTINGBOARD);
 
         registry.registerContainerClickArea(new Rectangle(88, 44, 26, 18), CookingPotScreen.class, COOKINGPOT);
+
+        registry.registerContainerClickArea(new Rectangle(88, 44, 26, 18), KegScreen.class, KEG);
     }
 }
---
+++ src/main/java/net/zuiron/photosynthesis/integration/rei/keg/KegRecipeCategory.java
@@ -0,0 +1,114 @@
+package net.zuiron.photosynthesis.integration.rei.keg;
+
+import dev.architectury.fluid.fabric.FluidStackImpl;
+import me.shedaniel.math.Point;
+import me.shedaniel.math.Rectangle;
+import me.shedaniel.rei.api.client.gui.Renderer;
+import me.shedaniel.rei.api.client.gui.widgets.Arrow;
+import me.shedaniel.rei.api.client.gui.widgets.Widget;
+import me.shedaniel.rei.api.client.gui.widgets.Widgets;
+import me.shedaniel.rei.api.client.registry.display.DisplayCategory;
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.entry.EntryIngredient;
+import me.shedaniel.rei.api.common.entry.EntryStack;
+import me.shedaniel.rei.api.common.util.EntryIngredients;
+import me.shedaniel.rei.api.common.util.EntryStacks;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.block.Block;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.state.property.Properties;
+import net.minecraft.text.Text;
+import net.minecraft.text.TextColor;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.collection.DefaultedList;
+import net.zuiron.photosynthesis.Photosynthesis;
+import net.zuiron.photosynthesis.block.ModBlocks;
+import net.zuiron.photosynthesis.integration.rei.PhotosynthesisREI;
+import net.zuiron.photosynthesis.integration.rei.cookingpot.CookingPotRecipeDisplay;
+import net.zuiron.photosynthesis.util.FluidStack;
+
+import java.util.*;
+
+@Environment(EnvType.CLIENT)
+public class KegRecipeCategory implements DisplayCategory<KegRecipeDisplay> {
+    private static final Identifier GUI_TEXTURE = new Identifier(Photosynthesis.MOD_ID, "textures/gui/keg_gui.png");
+
+    @Override
+    public Renderer getIcon() {
+        return EntryStacks.of(ModBlocks.KEG);
+    }
+
+    @Override
+    public Text getTitle() {
+        //return Photosynthesis.i18n("rei.cooking");
+        return Text.literal("Keg");
+    }
+
+    @Override
+    public CategoryIdentifier<? extends KegRecipeDisplay> getCategoryIdentifier() {
+        return PhotosynthesisREI.KEG;
+    }
+
+    @Override
+    public List<Widget> setupDisplay(KegRecipeDisplay display, Rectangle bounds) {
+        Point origin = bounds.getLocation();
+        final List<Widget> widgets = new ArrayList<>();
+
+        widgets.add(Widgets.createRecipeBase(bounds));
+        Rectangle bgBounds = PhotosynthesisREI.centeredIntoRecipeBase(origin, 122, 61);
+        widgets.add(Widgets.createTexturedWidget(GUI_TEXTURE, bgBounds, 27, 4));
+
+
+        List<EntryIngredient> ingredientEntries = display.getIngredientEntries();
+        DefaultedList counts = display.getCounts();
+
+
+        //input fluid
+        FluidStack inputFluid = display.getInputFluid();
+        widgets.add(Widgets.createSlot(new Point(bgBounds.x + 3, bgBounds.y + 41))
+                .entries(EntryIngredients.of(inputFluid.fluidVariant.getFluid(), inputFluid.amount)));
+
+        //output fluid
+        FluidStack outputFluid = display.getOutputFluid();
+        widgets.add(Widgets.createSlot(new Point(bgBounds.x + 103, bgBounds.y + 41))
+                .entries(EntryIngredients.of(outputFluid.fluidVariant.getFluid(), outputFluid.amount)));
+
+
+        //INPUTS
+        if (ingredientEntries != null) {
+            int[] posX = {26, 44, 62, 80};
+            int[] posY = {22, 22, 22, 22};
+
+            for (int i = 0; i < ingredientEntries.size(); i++) {
+                //INPUT ITEMS TODO foreach Item item = ((ItemStack) ingredientEntries.get(i) - might be multiple? if tags.
+                Item item = ((ItemStack) ingredientEntries.get(i).get(0).getValue()).getItem();
+                widgets.add(Widgets.createSlot(new Point(bgBounds.x + posX[i], bgBounds.y + posY[i])).markInput().entries(List.of(EntryStacks.of(new ItemStack(item, (Integer) counts.get(i))))));
+            }
+        }
+
+
+
+        int cookTime = display.getCookTime();
+        int cookTimeSeconds = cookTime / 20;
+        int cookTimeMinutes = cookTimeSeconds / 60;
+
+
+        Arrow cookArrow = Widgets.createArrow(new Point(bgBounds.x + 49, bgBounds.y + 43))
+                .animationDurationTicks(cookTime);
+        widgets.add(cookArrow);
+        widgets.add(Widgets.createLabel(new Point(
+                                cookArrow.getBounds().x + cookArrow.getBounds().width / 2 + 25, cookArrow.getBounds().y + 5),
+                        Text.literal(cookTimeSeconds + "s"))
+                .noShadow().centered().tooltip(Text.literal(cookTimeSeconds + "seconds ("+cookTime+" ticks) \n" + cookTimeMinutes + "minutes"))
+                .color(Formatting.DARK_GRAY.getColorValue(), Formatting.GRAY.getColorValue()));
+
+
+
+        return widgets;
+    }
+}
