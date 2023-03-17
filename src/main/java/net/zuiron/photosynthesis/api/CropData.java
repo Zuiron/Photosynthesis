1154e593cb326e45de434719af931b884f60e828
Zuiron <martinfloden90@gmail.com>
2023-03-17 05:14:45 +0100

    seasons

---
+++ src/main/java/net/zuiron/photosynthesis/api/CropData.java
@@ -0,0 +1,36 @@
+package net.zuiron.photosynthesis.api;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CropData {
+    private int[] maxAge;
+    private int[] minAge;
+
+    public CropData(int[] maxAge, int[] minAge) {
+        this.maxAge = maxAge;
+        this.minAge = minAge;
+    }
+
+    public int getMaxAge(int season) {
+        return maxAge[season]; //max crop age for 'season'
+    }
+
+    public int getMinAge(int season) {
+        return minAge[season]; //min crop age to grow in 'season'
+    }
+
+    public static Map<String, CropData> cropDataMap = new HashMap<>();
+    static { //maxAge Summer, Autumn, Winter, Spring ---- minAge Summer, Autumn, Winter, Spring
+        cropDataMap.put("block.minecraft.wheat", new CropData(new int[]{7, 1, 2, 5}, new int[]{4, 0, 1, 2}));
+        cropDataMap.put("block.minecraft.carrots", new CropData(new int[]{7, 1, 1, 4}, new int[]{4, 0, 1, 1}));
+        cropDataMap.put("block.minecraft.potatoes", new CropData(new int[]{7, 1, 1, 4}, new int[]{4, 0, 1, 1}));
+        cropDataMap.put("block.minecraft.beetroot", new CropData(new int[]{3, 0, 0, 2}, new int[]{2, 0, 1, 1}));
+        // add more crops here as needed
+    }
+
+    public static CropData getCropDataFor(String blockName) {
+        return cropDataMap.get(blockName);
+    }
+}
+
--- src/main/java/net/zuiron/photosynthesis/mixin/ModCropBlock.java
+++ src/main/java/net/zuiron/photosynthesis/mixin/ModCropBlock.java
@@ -1,17 +1,23 @@
 package net.zuiron.photosynthesis.mixin;
 
+import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.CropBlock;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import net.zuiron.photosynthesis.Photosynthesis;
+import net.zuiron.photosynthesis.api.CropData;
+import net.zuiron.photosynthesis.api.Seasons;
+import net.zuiron.photosynthesis.config.ModConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Objects;
 
 @Mixin(CropBlock.class)
 public abstract class ModCropBlock {
@@ -24,12 +30,41 @@ public abstract class ModCropBlock {
     //@Inject(method = "randomTick", at = @At("HEAD"))
     @Inject(method = "randomTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/CropBlock;withAge(I)Lnet/minecraft/block/BlockState;", ordinal = 0), cancellable = true)
     public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
-        Photosynthesis.LOGGER.info("apply growth?: "+state.getBlock().getTranslationKey());
-        //world.setBlockState(pos, this.withAge(this.getAge(state) + 1), 2); //works
-        if(state.getBlock().getTranslationKey() == "block.minecraft.wheat") {
 
+        if(Seasons.isSeasonsEnabled()) {
+            //Photosynthesis.LOGGER.info("apply growth?: " + state.getBlock().getTranslationKey());
+            /*int spring_maxAge = 4;
+            int spring_minAge = 1;
+
+            int summer_maxAge = 7;
+            int summer_minAge = 4;
+
+            int autumn_maxAge = 1;
+            int autumn_minAge = 0;
+
+            int winter_maxAge = 1;
+            int winter_minAge = 1;*/
+
+            //WHEAT - plant in autumn, harvestable in summer.
+            CropData cropData = CropData.getCropDataFor(state.getBlock().getTranslationKey());
+            if(cropData != null) {
+                int minAge = cropData.getMinAge(Seasons.getCurrentSeason(world.getTimeOfDay()));
+                int maxAge = cropData.getMaxAge(Seasons.getCurrentSeason(world.getTimeOfDay()));
+                int currentCropAge = this.getAge(state);
+                if(currentCropAge >= minAge && currentCropAge < maxAge) {
+                    Photosynthesis.LOGGER.info("Crop: "+state.getBlock().getTranslationKey()+", minAge:"+minAge+", maxAge:"+maxAge+", CurrentCropAge: "+currentCropAge+", NewCropAge: "+(this.getAge(state) + 1));
+                    world.setBlockState(pos, this.withAge(this.getAge(state) + 1), 2);
+                } else {
+                    Photosynthesis.LOGGER.info("Crop: "+state.getBlock().getTranslationKey()+", minAge:"+minAge+", maxAge:"+maxAge+", CurrentCropAge: "+currentCropAge+", NO GROW");
+                }
+            }
+
+            /*if (Objects.equals(state.getBlock().getTranslationKey(), "block.minecraft.wheat")) {
+                Photosynthesis.LOGGER.info("Wheat tried to grow. Season: "+Seasons.getCurrentSeason(world.getTimeOfDay()));
+            }*/
+            //world.setBlockState(pos, this.withAge(this.getAge(state) + 1), 2); //works - converted - applies growth
+            ci.cancel(); //cancel, do not run vanilla code.
         }
-        ci.cancel();
         //world.setBlockState(pos, this.withAge(i + 1), 2); //Original MC Code
     }
 
