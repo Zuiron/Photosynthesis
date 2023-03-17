989e8c426aa80b9ac6e0fc6be2ef687ed1a64684
Zuiron <martinfloden90@gmail.com>
2023-03-17 17:48:53 +0100

    seasons

--- src/main/java/net/zuiron/photosynthesis/mixin/ModCropBlock.java
+++ src/main/java/net/zuiron/photosynthesis/mixin/ModCropBlock.java
@@ -1,23 +1,20 @@
 package net.zuiron.photosynthesis.mixin;
 
-import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.CropBlock;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
+import net.minecraft.world.WorldView;
 import net.zuiron.photosynthesis.Photosynthesis;
 import net.zuiron.photosynthesis.api.CropData;
 import net.zuiron.photosynthesis.api.Seasons;
-import net.zuiron.photosynthesis.config.ModConfig;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.Objects;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(CropBlock.class)
 public abstract class ModCropBlock {
@@ -27,25 +24,17 @@ public abstract class ModCropBlock {
 
     @Shadow public abstract BlockState withAge(int age);
 
-    //@Inject(method = "randomTick", at = @At("HEAD"))
+    @Inject(method = "isFertilizable", at = @At("HEAD"), cancellable = true)
+    public void isFertilizable(WorldView world, BlockPos pos, BlockState state, boolean isClient, CallbackInfoReturnable<Boolean> cir) {
+        //Photosynthesis.LOGGER.info("bonemealing not allowed");
+        cir.setReturnValue(false);
+        cir.cancel();
+    }
+
     @Inject(method = "randomTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/CropBlock;withAge(I)Lnet/minecraft/block/BlockState;", ordinal = 0), cancellable = true)
     public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
 
         if(Seasons.isSeasonsEnabled()) {
-            //Photosynthesis.LOGGER.info("apply growth?: " + state.getBlock().getTranslationKey());
-            /*int spring_maxAge = 4;
-            int spring_minAge = 1;
-
-            int summer_maxAge = 7;
-            int summer_minAge = 4;
-
-            int autumn_maxAge = 1;
-            int autumn_minAge = 0;
-
-            int winter_maxAge = 1;
-            int winter_minAge = 1;*/
-
-            //WHEAT - plant in autumn, harvestable in summer.
             CropData cropData = CropData.getCropDataFor(state.getBlock().getTranslationKey());
             if(cropData != null) {
                 int minAge = cropData.getMinAge(Seasons.getCurrentSeason(world.getTimeOfDay()));
@@ -61,11 +50,6 @@ public abstract class ModCropBlock {
                 ci.cancel(); //cancel, do not run vanilla code.
                 //Note we only cancel IF we found seasons data for the crop. otherwise vanilla code runs.
             }
-
-            /*if (Objects.equals(state.getBlock().getTranslationKey(), "block.minecraft.wheat")) {
-                Photosynthesis.LOGGER.info("Wheat tried to grow. Season: "+Seasons.getCurrentSeason(world.getTimeOfDay()));
-            }*/
-            //world.setBlockState(pos, this.withAge(this.getAge(state) + 1), 2); //works - converted - applies growth
         }
         //world.setBlockState(pos, this.withAge(i + 1), 2); //Original MC Code
     }
