4e2620944947986edd49b28973d4ea0448894be8
Zuiron <martinfloden90@gmail.com>
2023-07-19 04:20:20 +0200

    get fluid from bucket item helper

---
+++ src/main/java/net/zuiron/photosynthesis/util/getFluidFromBucketItemHelper.java
@@ -0,0 +1,8 @@
+package net.zuiron.photosynthesis.util;
+
+import net.minecraft.fluid.Fluid;
+import net.minecraft.nbt.NbtCompound;
+
+public interface getFluidFromBucketItemHelper {
+    Fluid getFluidFromBucketItem();
+}
