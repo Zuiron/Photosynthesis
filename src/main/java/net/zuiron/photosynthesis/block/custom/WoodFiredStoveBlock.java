5dd271f41e172270b1de3dbc42648b60679a8346
Zuiron <martinfloden90@gmail.com>
2023-07-26 18:59:56 +0200

    wood fired stove p6

--- src/main/java/net/zuiron/photosynthesis/block/custom/WoodFiredStoveBlock.java
+++ src/main/java/net/zuiron/photosynthesis/block/custom/WoodFiredStoveBlock.java
@@ -123,9 +123,9 @@ public class WoodFiredStoveBlock extends BlockWithEntity implements BlockEntityP
         Direction.Axis axis = direction.getAxis();
         double g = 0.52;
         double h = random.nextDouble() * 0.6 - 0.3;
-        double i = axis == Direction.Axis.X ? (double)direction.getOffsetX() * 0.32 : h; //0.52
+        double i = axis == Direction.Axis.X ? (double)direction.getOffsetX() * 0.22 : h; //0.52
         double j = random.nextDouble() * 6.0 / 16.0;
-        double k = axis == Direction.Axis.Z ? (double)direction.getOffsetZ() * 0.32 : h; //0.52
+        double k = axis == Direction.Axis.Z ? (double)direction.getOffsetZ() * 0.22 : h; //0.52
         world.addParticle(ParticleTypes.SMOKE, d + i, e + j, f + k, 0.0, 0.0, 0.0);
         world.addParticle(ParticleTypes.FLAME, d + i, e + j, f + k, 0.0, 0.0, 0.0);
     }
