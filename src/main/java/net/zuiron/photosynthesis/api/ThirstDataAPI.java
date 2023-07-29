1c991a7607bb6d71766eae84f4b6f20e8e84c92f
Zuiron <martinfloden90@gmail.com>
2023-07-29 19:09:16 +0200

    thirstAPI

--- src/main/java/net/zuiron/photosynthesis/api/ThirstDataAPI.java
+++ src/main/java/net/zuiron/photosynthesis/api/ThirstDataAPI.java
@@ -1,28 +1,44 @@
 package net.zuiron.photosynthesis.api;
 
+import me.shedaniel.autoconfig.AutoConfig;
+import net.zuiron.photosynthesis.config.ModConfig;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class ThirstDataAPI {
+    static ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
     private int thirst;
     private int thirstSat;
+    private int remThirst;
+    private int remThirstSat;
 
-    public ThirstDataAPI(int thirst, int thirstSat) {
+    public ThirstDataAPI(int thirst, int thirstSat, int remThirst, int remThirstSat) {
         this.thirst = thirst;
         this.thirstSat = thirstSat;
+        this.remThirst = remThirst;
+        this.remThirstSat = remThirstSat;
     }
 
     public int getThirstData() {
         return thirst;
     }
 
+    public int getRemThirstData() {return remThirst; }
+
     public int getThirstSatData() {
         return thirstSat;
     }
 
+    public int getRemThirstSatData() {return remThirstSat; }
+
+    public static boolean isThirstEnabled() { return config.thirst; }
+
     public static Map<String, ThirstDataAPI> thirstDataMap = new HashMap<>();
     static {
-        thirstDataMap.put("item.photosynthesis.strawberry",  new ThirstDataAPI(0, 60));
+        //IF a food item is NOT in this list. you will lose 1 Thirst, and 150 Saturation.
+        thirstDataMap.put("item.minecraft.cooked_beef",  new ThirstDataAPI(0, 0, 2, 300));
+        thirstDataMap.put("item.photosynthesis.strawberry",  new ThirstDataAPI(0, 60, 0, 0));
     }
 
     public static ThirstDataAPI getThirstDataFor(String itemName) {
--- src/main/java/net/zuiron/photosynthesis/event/OnPlayerEatEvent.java
+++ src/main/java/net/zuiron/photosynthesis/event/OnPlayerEatEvent.java
@@ -3,6 +3,8 @@ package net.zuiron.photosynthesis.event;
 import net.minecraft.text.Text;
 import net.zuiron.photosynthesis.api.CropData;
 import net.zuiron.photosynthesis.api.ThirstDataAPI;
+import net.zuiron.photosynthesis.util.IEntityDataSaver;
+import net.zuiron.photosynthesis.util.ThirstData;
 
 public class OnPlayerEatEvent {
     public static void registerOnPlayerEatEvents() {
@@ -13,11 +15,29 @@ public class OnPlayerEatEvent {
     public static void ModOnPlayerEatEventThirstHandler() {
         PlayerEatEvent.EVENT.register((world, player, stack) -> {
             if(!world.isClient) {
-                ThirstDataAPI thirstDataAPI = ThirstDataAPI.getThirstDataFor(stack.getItem().getTranslationKey());
-                if(thirstDataAPI != null) {
-                    int thirst = thirstDataAPI.getThirstData();
-                    int thirstSat = thirstDataAPI.getThirstSatData();
+                ThirstDataAPI thirstItemData = ThirstDataAPI.getThirstDataFor(stack.getItem().getTranslationKey());
+                boolean isThirstEnabled = ThirstDataAPI.isThirstEnabled();
+                if(thirstItemData != null && isThirstEnabled) {
+                    int thirst = thirstItemData.getThirstData();
+                    int thirstSat = thirstItemData.getThirstSatData();
+
+                    int remThirst = thirstItemData.getRemThirstData();
+                    int remThirstSat = thirstItemData.getRemThirstSatData();
+
+                    //ADD thirst.
+                    ThirstData.addThirst((IEntityDataSaver)player, thirst);
+                    ThirstData.addThirstSaturation((IEntityDataSaver)player, thirstSat);
+
+                    //REMOVE thirst.
+                    ThirstData.removeThirst((IEntityDataSaver)player, remThirst);
+                    ThirstData.removeThirstSaturation((IEntityDataSaver)player, remThirstSat);
+
                     player.sendMessage(Text.of("We have thirst data for this: thirst:"+thirst +", sat:"+thirstSat + " - " + stack.getItem().getTranslationKey()), false);
+                }
+                else if (thirstItemData == null && isThirstEnabled) {
+                    //DEFAULT remove thirst, unknown item. remove 2 thirst bars.
+                    ThirstData.removeThirst((IEntityDataSaver)player, 1);
+                    ThirstData.removeThirstSaturation((IEntityDataSaver)player, 150);
                 }
             }
         });
@@ -26,7 +46,7 @@ public class OnPlayerEatEvent {
     public static void ModOnPlayerEatEvent() {
         PlayerEatEvent.EVENT.register((world, player, stack) -> {
             if(!world.isClient) {
-                player.sendMessage(Text.of("You just ate: " + stack.getItem().getName().getString()), false);
+                player.sendMessage(Text.of("You just ate: " + stack.getItem().getName().getString()+" "+stack.getItem().getTranslationKey()), false);
             }
         });
     }
