f168e5dd22593f237c3bd479a1a28df1920692be
Zuiron <martinfloden90@gmail.com>
2023-08-06 17:35:08 +0200

    mob spawn prevention.

--- src/main/java/net/zuiron/photosynthesis/mixin/ModAnimalEntitySpawnRestrictor.java
+++ src/main/java/net/zuiron/photosynthesis/mixin/ModAnimalEntitySpawnRestrictor.java
@@ -1,8 +1,10 @@
 package net.zuiron.photosynthesis.mixin;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.predicate.entity.EntityTypePredicate;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.WorldAccess;
@@ -12,19 +14,29 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Objects;
+
 @Mixin(AnimalEntity.class)
 public class ModAnimalEntitySpawnRestrictor {
     @Inject(method = "isValidNaturalSpawn", at = @At("HEAD"), cancellable = true)
     private static void isValidNaturalSpawn(EntityType<? extends AnimalEntity> type, WorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random, CallbackInfoReturnable<Boolean> cir) {
+        //vanilla code
         //return world.getBlockState(pos.down()).isIn(BlockTags.ANIMALS_SPAWNABLE_ON) && AnimalEntity.isLightLevelValidForNaturalSpawn(world, pos);
-        /*if(type == EntityType.PIG) {
-            Photosynthesis.LOGGER.info("type: "+type+", reason: "+spawnReason);
-        }*/
 
-        if(type == EntityType.PIG && spawnReason != SpawnReason.SPAWN_EGG || spawnReason != SpawnReason.BREEDING) {
-            //Photosynthesis.LOGGER.info("Prevented PIG from spawning, it's not from breeding, or spawn-egg!");
-            cir.setReturnValue(false);
-            cir.cancel();
+        Photosynthesis.LOGGER.info("trying spawning: "+type+" at: "+pos+", reason: "+spawnReason);
+        if(     type.equals(EntityType.PIG) ||
+                type.equals(EntityType.COW) ||
+                type.equals(EntityType.SHEEP) ||
+                type.equals(EntityType.CHICKEN) ||
+                type.equals(EntityType.HORSE) ||
+                type.equals(EntityType.GOAT)) {
+            if(spawnReason == SpawnReason.SPAWN_EGG || spawnReason == SpawnReason.BREEDING) {
+                cir.setReturnValue(true);
+            } else {
+                Photosynthesis.LOGGER.info("Prevented " + type + " from spawning @" + pos + ", it's not from breeding, or spawn-egg!");
+                cir.setReturnValue(false);
+            }
         }
+
     }
 }
---
+++ src/main/java/net/zuiron/photosynthesis/mixin/ModMobEntity.java
@@ -0,0 +1,43 @@
+package net.zuiron.photosynthesis.mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.dimension.DimensionTypes;
+import net.zuiron.photosynthesis.Photosynthesis;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MobEntity.class)
+public class ModMobEntity {
+
+    @Inject(method = "canMobSpawn", at = @At("HEAD"), cancellable = true)
+    private static void canMobSpawn(EntityType<? extends MobEntity> type, WorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random, CallbackInfoReturnable<Boolean> cir) {
+        //vanilla code.
+        //BlockPos blockPos = pos.down();
+        //return spawnReason == SpawnReason.SPAWNER || world.getBlockState(blockPos).allowsSpawning(world, blockPos, type);
+
+        Photosynthesis.LOGGER.info("trying spawning: "+type+" at: "+pos+", reason: "+spawnReason);
+        if(     type.equals(EntityType.CREEPER) ||
+                type.equals(EntityType.ZOMBIE) ||
+                type.equals(EntityType.ZOMBIE_VILLAGER) ||
+                type.equals(EntityType.ZOMBIE_HORSE) ||
+                type.equals(EntityType.SKELETON) ||
+                type.equals(EntityType.SKELETON_HORSE)) {
+            if(spawnReason == SpawnReason.CHUNK_GENERATION || spawnReason == SpawnReason.NATURAL) {
+                Photosynthesis.LOGGER.info("Prevented " + type + " from spawning @" + pos + ", it's from chunkgen, or natural reasons!");
+                cir.setReturnValue(false);
+            } else {
+                //allow structure and spawnegg, spawners etc. but not natural or chunk gen.
+                BlockPos blockPos = pos.down();
+                cir.setReturnValue(spawnReason == SpawnReason.SPAWNER || world.getBlockState(blockPos).allowsSpawning(world, blockPos, type));
+            }
+        }
+    }
+}
