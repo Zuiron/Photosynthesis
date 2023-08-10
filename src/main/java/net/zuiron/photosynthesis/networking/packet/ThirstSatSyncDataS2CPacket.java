e2408e60173c6bd02a86063674dc434f8bcc76ef
Zuiron <martinfloden90@gmail.com>
2023-08-10 21:40:27 +0200

    fix

--- src/main/java/net/zuiron/photosynthesis/networking/packet/ThirstSatSyncDataS2CPacket.java
+++ src/main/java/net/zuiron/photosynthesis/networking/packet/ThirstSatSyncDataS2CPacket.java
@@ -9,6 +9,7 @@ import net.zuiron.photosynthesis.util.IEntityDataSaver;
 public class ThirstSatSyncDataS2CPacket {
     public static void receive(MinecraftClient client, ClientPlayNetworkHandler handler,
                                PacketByteBuf buf, PacketSender responseSender) {
+        assert client.player != null;
         ((IEntityDataSaver) client.player).getPersistentData().putInt("thirst_sat", buf.readInt());
     }
 }
