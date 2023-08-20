32f1208ed346c57135230f67a19faead76887371
Zuiron <martinfloden90@gmail.com>
2023-08-21 01:33:14 +0200

    fix

--- src/main/java/net/zuiron/photosynthesis/block/custom/ModSilageBaleBlock.java
+++ src/main/java/net/zuiron/photosynthesis/block/custom/ModSilageBaleBlock.java
@@ -1,9 +1,19 @@
 package net.zuiron.photosynthesis.block.custom;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
 public class ModSilageBaleBlock extends ModBaleBlock{
     public ModSilageBaleBlock(Settings settings) {
         super(settings);
     }
 
-
+    @Override
+    public @Nullable <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
+        return null;
+    }
 }
