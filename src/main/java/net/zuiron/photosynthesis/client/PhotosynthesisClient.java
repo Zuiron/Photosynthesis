18083df6e9409b51ff85e1568002421a2efcaa67
Zuiron <martinfloden90@gmail.com>
2023-08-25 20:02:29 +0200

    egg basket fix

--- src/main/java/net/zuiron/photosynthesis/client/PhotosynthesisClient.java
+++ src/main/java/net/zuiron/photosynthesis/client/PhotosynthesisClient.java
@@ -401,6 +401,7 @@ public class PhotosynthesisClient implements ClientModInitializer {
         BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), ModBlocks.WOOD_FIRED_OVEN);
         BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), ModBlocks.GRAVITYPRESS);
         BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), ModBlocks.EGGBASKET);
+        BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), ModBlocks.SEASONSCALENDAR);
 
         HandledScreens.register(ModScreenHandlers.CUTTING_BOARD_SCREEN_HANDLER, CuttingBoardScreen::new);
         HandledScreens.register(ModScreenHandlers.MORTAR_SCREEN_HANDLER, MortarScreen::new);
