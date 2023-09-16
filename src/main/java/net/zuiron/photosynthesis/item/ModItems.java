c01b7fe9d36292af39aa4c38dab8ccdc1bf702af
Zuiron <martinfloden90@gmail.com>
2023-09-16 15:34:15 +0200

    fix #213

--- src/main/java/net/zuiron/photosynthesis/item/ModItems.java
+++ src/main/java/net/zuiron/photosynthesis/item/ModItems.java
@@ -1244,7 +1244,7 @@ public class ModItems {
                     .food(new FoodComponent.Builder().hunger(1).saturationModifier(0.1f).build())));
 
     public static final Item PUMPKIN_SOUP = registerItem("pumpkin_soup",
-            new Item(new FabricItemSettings()
+            new StackableStewItem(new FabricItemSettings()
                     .food(new FoodComponent.Builder().hunger(10).saturationModifier(0.5f).build())));
 
     /*public static final Item WAFFLE = registerItem("waffle",
