43d627786c76612bf0deb1de4ba97b301ea8f232
Zuiron <martinfloden90@gmail.com>
2023-09-20 15:50:07 +0200

    better boar spawn

--- src/main/java/net/zuiron/photosynthesis/world/gen/ModEntitySpawns.java
+++ src/main/java/net/zuiron/photosynthesis/world/gen/ModEntitySpawns.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.entity.SpawnRestriction;
 import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.registry.tag.BiomeTags;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.biome.BiomeKeys;
 import net.zuiron.photosynthesis.entity.ModEntities;
@@ -12,7 +13,13 @@ import net.zuiron.photosynthesis.entity.custom.BoarEntity;
 
 public class ModEntitySpawns {
     public static void addSpawns() {
-        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.JUNGLE, BiomeKeys.FOREST, BiomeKeys.WOODED_BADLANDS, BiomeKeys.DARK_FOREST),
+        /*BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.JUNGLE, BiomeKeys.FOREST, BiomeKeys.WOODED_BADLANDS, BiomeKeys.DARK_FOREST),
+                SpawnGroup.CREATURE, ModEntities.BOAR, 200, 1, 3);*/
+        BiomeModifications.addSpawn(BiomeSelectors.tag(BiomeTags.IS_BADLANDS),
+                SpawnGroup.CREATURE, ModEntities.BOAR, 200, 1, 3);
+        BiomeModifications.addSpawn(BiomeSelectors.tag(BiomeTags.IS_FOREST),
+                SpawnGroup.CREATURE, ModEntities.BOAR, 200, 1, 3);
+        BiomeModifications.addSpawn(BiomeSelectors.tag(BiomeTags.IS_JUNGLE),
                 SpawnGroup.CREATURE, ModEntities.BOAR, 200, 1, 3);
 
         SpawnRestriction.register(ModEntities.BOAR, SpawnRestriction.Location.ON_GROUND,
